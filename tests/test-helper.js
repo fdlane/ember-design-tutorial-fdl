d17e1ca78d1139c6cf9a36e470dc80bafefee077
fdlane <fdlane@hotmail.com>
2025-02-07 16:47:06 -0500

    chore: Init Ember app

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-design-tutorial-fdl/app';
+import config from 'ember-design-tutorial-fdl/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
