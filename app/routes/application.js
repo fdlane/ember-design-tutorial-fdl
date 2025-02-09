2b51aba3e25408f5f77dcc0ed3f4d5af59d4942a
fdlane <fdlane@hotmail.com>
2025-02-09 07:43:34 -0500

    feat: Add header and theme switcher

---
+++ app/routes/application.js
@@ -0,0 +1,12 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+
+export default class ApplicationRoute extends Route {
+  @service
+  theme;
+
+  beforeModel() {
+    this.theme.setTheme('dark');
+    this.theme.load();
+  }
+}
