1454701d8566431afeffab877418a3d1b1ed52ef
fdlane <fdlane@hotmail.com>
2025-02-09 07:52:11 -0500

    feat: Add Greeting component

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,9 +3,13 @@ import { service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
   @service
+  intl;
+
+  @service
   theme;
 
   beforeModel() {
+    this.intl.setLocale('en-us');
     this.theme.setTheme('dark');
     this.theme.load();
   }
